ad95a183b5b4423813e62330ba351e56aebbc9d4
Петрушина Кристина Алексеевна <k.petrushina@etpgpb.ru>
2024-01-29 10:03:53 +0300

    add-jwt-auth

--- src/Controller/Security/TokenController.php
+++ src/Controller/Security/TokenController.php
@@ -10,16 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TokenController extends AbstractController
 {
-    #[Route('/login', name: 'login')]
-    public function generateToken(Request $request, JWTTokenManagerInterface $jwtManager): JsonResponse
-    {
-        $payload = [
-            'sub' => 'anonymous', // Уникальный идентификатор анонимного пользователя
-            'exp' => (new \DateTime('+1 hour'))->getTimestamp() // Время истечения токена
-        ];
-
-        $token = $jwtManager->create($payload);
-
-        return new JsonResponse(['token' => $token]);
-    }
+//    #[Route('/login', name: 'login')]
+//    public function generateToken(Request $request, JWTTokenManagerInterface $jwtManager): JsonResponse
+//    {
+//        $payload = [
+//            'sub' => 'anonymous', // Уникальный идентификатор анонимного пользователя
+//            'exp' => (new \DateTime('+1 hour'))->getTimestamp() // Время истечения токена
+//        ];
+//
+//        $token = $jwtManager->create($payload);
+//
+//        return new JsonResponse(['token' => $token]);
+//    }
 }
